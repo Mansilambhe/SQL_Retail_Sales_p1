9f5751f0ee8fa5b59369883a2a1b67f995b3803c
Mansilambhe <161935308+Mansilambhe@users.noreply.github.com>
2026-03-21 16:28:17 +0530

    Add files via upload

--- SQL_P1.sql
+++ SQL_P1.sql
@@ -1,4 +1,20 @@
+--SQL Retail Sales Analysis --
+CREATE DATABASE sql_project_p2;
 
+CREATE TABLE retail_sales
+              (
+               transactions_id INT PRIMARY KEY,
+			   sale_date DATE,	
+               sale_time TIME,	
+               customer_id	INT,
+			   gender VARCHAR(15),
+               age INT,	
+               category VARCHAR(15),	
+               quantiy INT,
+               price_per_unit FLOAT,	
+               cogs	FLOAT,
+               total_sale FLOAT
+			  );
 
 select * from retail_sales;
 
